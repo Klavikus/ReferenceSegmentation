ce5c064c595f1781354b7c791715a631dc1538fd
Klavikus <v.sorokin.dev@gmail.com>
2024-10-04 01:34:04 +0300

    Improve result visualization

--- gradio_gui.py
+++ gradio_gui.py
@@ -5,10 +5,12 @@ from segmentation_processor import WatershedProcessor
 
 class GradioGui:
     def __init__(self, width: int = 512, height: int = 512):
+        self.processor = None
         self.width = width
         self.height = height
 
     def run(self, processor: WatershedProcessor, share: bool = False):
+        self.processor = processor
         with gr.Blocks(css="""
             .gradio-container {
                 max-width: 1200px;
@@ -68,12 +70,30 @@ class GradioGui:
                             value=True,
                             elem_id="auto_update")
                         run_btn = gr.Button("Рассчитать", elem_id="run_btn")
-                    result_image = gr.Image(
-                        type="numpy",
-                        label="Результат",
-                        elem_id="result_image",
-                        height=self.height,
-                        width=self.width)
+
+                    with gr.Tabs():
+                        with gr.TabItem("Контуры"):
+                            result_image_contours = gr.Image(
+                                type="numpy",
+                                label="Результат (Контуры)",
+                                elem_id="result_image_contours",
+                                height=self.height,
+                                width=self.width)
+                        with gr.TabItem("Покрас по нормали"):
+                            result_image_normal = gr.Image(
+                                type="numpy",
+                                label="Результат (Покрас по нормали)",
+                                elem_id="result_image_normal",
+                                height=self.height,
+                                width=self.width)
+                        with gr.TabItem("Покрас с персентилями"):
+                            result_image_percentile = gr.Image(
+                                type="numpy",
+                                label="Результат (Покрас с персентилями)",
+                                elem_id="result_image_percentile",
+                                height=self.height,
+                                width=self.width)
+
                     data_table = gr.Dataframe(
                         label="Данные сегментированных объектов",
                         headers=["ID", "Площадь", "Периметр", "Центр X", "Центр Y"],
@@ -88,31 +108,41 @@ class GradioGui:
                         download_excel_overall = gr.File(label="Скачать Excel Файл")
 
             prepare_btn.click(
-                fn=processor.prepare_image,
+                fn=self.__handle_prepare_image,
                 inputs=input_image,
                 outputs=[output_image, mask_input_f, mask_input_b]
             )
 
             run_btn.click(
-                fn=processor.watershed_algorithm,
+                fn=self.__handle_watershed_algorithm,
                 inputs=[output_image, mask_input_f, mask_input_b],
-                outputs=[result_image, data_table, download_excel, data_table_overall, download_excel_overall]
+                outputs=[result_image_contours, result_image_normal, result_image_percentile, data_table,
+                         download_excel, data_table_overall, download_excel_overall]
             )
 
-            def auto_run_processing(auto_update_flag, output_img, mask_f, mask_b):
-                if auto_update_flag:
-                    return processor.watershed_algorithm(output_img, mask_f, mask_b)
-
             mask_input_f.change(
-                fn=auto_run_processing,
+                fn=self.__auto_run_processing,
                 inputs=[auto_update, output_image, mask_input_f, mask_input_b],
-                outputs=[result_image, data_table, download_excel, data_table_overall, download_excel_overall]
+                outputs=[result_image_contours, result_image_normal, result_image_percentile, data_table,
+                         download_excel, data_table_overall, download_excel_overall]
             )
 
             mask_input_b.change(
-                fn=auto_run_processing,
+                fn=self.__auto_run_processing,
                 inputs=[auto_update, output_image, mask_input_f, mask_input_b],
-                outputs=[result_image, data_table, download_excel, data_table_overall, download_excel_overall]
+                outputs=[result_image_contours, result_image_normal, result_image_percentile, data_table,
+                         download_excel, data_table_overall, download_excel_overall]
             )
 
             demo.launch(share=share, debug=True)
+
+    def __handle_prepare_image(self, input_image):
+        output_image = self.processor.prepare_image(input_image)
+        return output_image, output_image, output_image
+
+    def __handle_watershed_algorithm(self, output_image, mask_input_f, mask_input_b):
+        return self.processor.watershed_algorithm(output_image, mask_input_f, mask_input_b)
+
+    def __auto_run_processing(self, auto_update_flag, output_img, mask_f, mask_b):
+        if auto_update_flag:
+            return self.__handle_watershed_algorithm(output_img, mask_f, mask_b)
--- segmentation_processor.py
+++ segmentation_processor.py
@@ -16,62 +16,28 @@ class WatershedProcessor:
         self.height = height
         self.pixel_to_nm = pixel_to_nm
 
-    def preprocess_image(self, image: np.ndarray) -> Any | None:
+    def prepare_image(self, image: np.ndarray) -> ndarray:
         if image is None:
-            return None, 0, 0
-        h, w = image.shape[:2]
-        scale = min(self.width / w, self.height / h)
-        new_w, new_h = int(w * scale), int(h * scale)
-        resized = cv2.resize(image, (new_w, new_h))
-
-        x_offset = (self.width - new_w) // 2
-        y_offset = (self.height - new_h) // 2
-
-        if len(image.shape) == 2:
-            padded = np.full((self.height, self.width), 255, dtype=image.dtype)
-            padded[y_offset:y_offset + new_h, x_offset:x_offset + new_w] = resized
-        else:
-            padded = np.full((self.height, self.width, 3), 255, dtype=image.dtype)
-            padded[y_offset:y_offset + new_h, x_offset:x_offset + new_w, :] = resized
-
-        return padded, x_offset, y_offset
-
-    def prepare_image(self, image: np.ndarray) -> tuple:
-        if image is None:
-            return None, None, None, None, None
-        resized_image, _, _ = self.preprocess_image(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+            raise ValueError("Image is None")
+        resized_image, _, _ = self.__preprocess_image(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
         gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
         gray_rgb = cv2.cvtColor(gray_bgr, cv2.COLOR_BGR2RGB)
-        return gray_rgb, gray_rgb, gray_rgb
+        return gray_rgb
 
     def watershed_algorithm(self, image: np.ndarray, mask_data_f: dict, mask_data_b: dict) -> tuple:
         if image is None or mask_data_f is None or mask_data_b is None:
-            return "Пожалуйста, сначала загрузите изображение и нарисуйте маски.", None, None, None, None
-
+            raise ValueError("Missing image or mask data")
         if 'layers' not in mask_data_f or not mask_data_f['layers']:
-            return "Пожалуйста, нарисуйте маску в Mask F.", None, None, None, None
+            raise ValueError("No Mask F layers provided")
         if 'layers' not in mask_data_b or not mask_data_b['layers']:
-            return "Пожалуйста, нарисуйте маску в Mask B.", None, None, None, None
-
-        mask_sf = mask_data_f['layers'][0]
-        if mask_sf is None:
-            return "Пожалуйста, нарисуйте маску в Mask F.", None, None, None, None
-
-        mask_sb = mask_data_b['layers'][0]
-        if mask_sb is None:
-            return "Пожалуйста, нарисуйте маску в Mask B.", None, None, None, None
-
-        image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        mask_sf = np.array(mask_sf)
+            raise ValueError("No Mask B layers provided")
+        mask_sf = np.array(mask_data_f['layers'][0])
+        mask_sb = np.array(mask_data_b['layers'][0])
         mask_sf = cv2.cvtColor(mask_sf, cv2.COLOR_RGBA2GRAY)
-
-        mask_sb = np.array(mask_sb)
         mask_sb = cv2.cvtColor(mask_sb, cv2.COLOR_RGBA2GRAY)
-
-        mask_sf, x_offset, y_offset = self.preprocess_image(mask_sf)
-        mask_sb, _, _ = self.preprocess_image(mask_sb)
+        mask_sf, x_offset, y_offset = self.__preprocess_image(mask_sf)
+        mask_sb, _, _ = self.__preprocess_image(mask_sb)
 
         _, sure_fg = cv2.threshold(mask_sf, 0, 255, cv2.THRESH_BINARY)
         _, sure_bg = cv2.threshold(mask_sb, 0, 255, cv2.THRESH_BINARY)
@@ -80,34 +46,84 @@ class WatershedProcessor:
         markers[sure_bg > 0] = 1
         markers[sure_fg > 0] = 255
 
-        if len(image_bgr.shape) == 2:
-            image_bgr = cv2.cvtColor(image_bgr, cv2.COLOR_GRAY2BGR)
-
+        image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         markers = cv2.watershed(image_bgr, markers)
         result = image_bgr.copy()
 
         unique_markers = np.unique(markers)
         unique_markers = unique_markers[(unique_markers != 1) & (unique_markers != -1)]
+        data, result_contours, result_colored_percentile, result_colored_normalized = self.__calculate_object_properties(unique_markers, markers, result,
+                                                                                   image_bgr, x_offset,
+                                                                                   y_offset)
 
+        if not data:
+            raise ValueError("No objects found")
+
+        df = pd.DataFrame(data)
+        excel_path = "output.xlsx"
+        df.to_excel(excel_path, index=False, sheet_name='Сегментированные объекты')
+
+        if not Path(excel_path).is_file():
+            raise ValueError("Error saving Excel file")
+
+        excel_path_overall = "overall.xlsx"
+        total_particles = len(data)
+        avg_area = df['Площадь'].mean()
+        avg_perimeter = df['Периметр'].mean()
+        overall_data = {
+            'Общее кол-во частиц': [total_particles],
+            'Средняя площадь': [avg_area],
+            'Средний периметр': [avg_perimeter]
+        }
+        df_overall = pd.DataFrame(overall_data)
+        df_overall.to_excel(excel_path_overall, index=False, sheet_name='Общие данные')
+
+        if not Path(excel_path_overall).is_file():
+            raise ValueError("Error saving overall Excel file")
+
+        return result_contours, result_colored_percentile, result_colored_normalized, df, excel_path, df_overall, excel_path_overall
+
+    def __preprocess_image(self, image: np.ndarray) -> Any:
+        if image is None:
+            raise ValueError("Image is None")
+        h, w = image.shape[:2]
+        scale = min(self.width / w, self.height / h)
+        new_w, new_h = int(w * scale), int(h * scale)
+        resized = cv2.resize(image, (new_w, new_h))
+        x_offset = (self.width - new_w) // 2
+        y_offset = (self.height - new_h) // 2
+        padded = self.__resize_and_pad_image(resized, x_offset, y_offset)
+        return padded, x_offset, y_offset
+
+    def __resize_and_pad_image(self, image: np.ndarray, x_offset: int, y_offset: int) -> np.ndarray:
+        if len(image.shape) == 2:
+            padded = np.full((self.height, self.width), 255, dtype=image.dtype)
+            padded[y_offset:y_offset + image.shape[0], x_offset:x_offset + image.shape[1]] = image
+        else:
+            padded = np.full((self.height, self.width, 3), 255, dtype=image.dtype)
+            padded[y_offset:y_offset + image.shape[0], x_offset:x_offset + image.shape[1], :] = image
+        return padded
+
+    def __calculate_object_properties(self, unique_markers, markers, result, image_bgr, x_offset, y_offset) -> tuple:
         data = []
         current_object_id = 1
+        result_contours = result.copy()
+        result_colored_percentile = result.copy()
+        result_colored_normalized = result.copy()
 
         for marker in unique_markers:
             obj_mask = np.uint8(markers == marker)
-
             num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(obj_mask, connectivity=8)
 
             for label in range(1, num_labels):  # label=0 это фон внутри маски
                 component_mask = np.uint8(labels == label)
-                mask = labels == label
-
                 contours, _ = cv2.findContours(component_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
                 if contours:
                     contour = contours[0]
+                    m = cv2.moments(contour)
                     area = np.sum(component_mask) * self.pixel_to_nm
                     perimeter = cv2.arcLength(contour, True)
-
-                    m = cv2.moments(contour)
                     if m["m00"] != 0:
                         c_x = int(m["m10"] / m["m00"])
                         c_y = int(m["m01"] / m["m00"])
@@ -125,73 +141,49 @@ class WatershedProcessor:
                         "Центр Y": c_y_adj
                     })
 
+                    mask = labels == label
                     object_image = np.zeros_like(image_bgr)
                     object_image[mask] = image_bgr[mask]
-
-                    colored_object = self.assigne_color_with_normilize(object_image, mask)
+                    colored_object_percentile = self.__coloring_with_percentile(object_image, mask)
+                    colored_object_normalized = self.__coloring_with_normilized(object_image, mask)
                     alpha = 0.35
-                    result[mask] = cv2.addWeighted(result[mask], 1 - alpha, colored_object[mask], alpha, 0)
 
-                    cv2.drawContours(result, [contour], -1, (0, 0, 255), 1)
-                    cv2.circle(result, (c_x, c_y), 3, (0, 255, 0), -1)
+                    result_colored_percentile[mask] = cv2.addWeighted(result_colored_percentile[mask], 1 - alpha,
+                                                                      colored_object_percentile[mask], alpha,
+                                                                      0)
+                    result_colored_normalized[mask] = cv2.addWeighted(result_colored_normalized[mask], 1 - alpha,
+                                                                      colored_object_normalized[mask], alpha,
+                                                                      0)
 
+                    cv2.drawContours(result_contours, [contour], -1, (0, 0, 255), 1)
+                    cv2.circle(result_contours, (c_x, c_y), 3, (0, 255, 0), -1)
                     current_object_id += 1
+        return data, result_contours, result_colored_percentile, result_colored_normalized
 
-        if not data:
-            return "Объекты не обнаружены.", None, None, None, None
-
-        df = pd.DataFrame(data)
-
-        excel_path = "output.xlsx"
-        with pd.ExcelWriter(excel_path, engine='xlsxwriter') as writer:
-            df.to_excel(writer, index=False, sheet_name='Сегментированные объекты')
-
-        if not Path(excel_path).is_file():
-            return "Ошибка при сохранении Excel файла.", None, None, None, None
-
-        excel_path_overall = "overall.xlsx"
-        total_particles = len(data)
-        avg_area = df['Площадь'].mean()
-        avg_perimeter = df['Периметр'].mean()
-
-        overall_data = {
-            'Общее кол-во частиц': [total_particles],
-            'Средняя площадь': [avg_area],
-            'Средний периметр': [avg_perimeter]
-        }
-        df_overall = pd.DataFrame(overall_data)
-
-        with pd.ExcelWriter(excel_path_overall, engine='xlsxwriter') as writer:
-            df_overall.to_excel(writer, index=False, sheet_name='Общие данные')
-
-        if not Path(excel_path_overall).is_file():
-            return "Ошибка при сохранении общего Excel файла.", None, None, None, None
-
-        return result, df, excel_path, df_overall, excel_path_overall
-
-    def assigne_color_with_normilize(self, image: np.ndarray, mask: bool) -> np.ndarray | None:
+    def __coloring_with_percentile(self, image: np.ndarray, mask: bool) -> np.ndarray:
         if image is None:
-            return None
-
+            raise ValueError("Image is None")
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        percentiles = np.percentile(gray[mask], [25, 50, 75, 95])
+        colors = [[255, 0, 0], [255, 165, 0], [255, 255, 0], [0, 255, 0]]
+        colored_image = np.zeros_like(image)
+        for i in range(len(percentiles) - 1):
+            range_mask = (gray >= percentiles[i]) & (gray < percentiles[i + 1])
+            colored_image[range_mask] = colors[i]
+        return colored_image
 
+    def __coloring_with_normilized(self, image: np.ndarray, mask: bool) -> np.ndarray:
+        if image is None:
+            raise ValueError("Image is None")
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         min_val = np.min(gray[mask])
         max_val = np.max(gray[mask])
         normalized = np.clip((gray - min_val) / (max_val - min_val), 0, 1)
-
         n = 4
         thresholds = np.linspace(0, 1, n + 1)
-        colors = [
-            [255, 0, 0],  # Red
-            [255, 165, 0],  # Orange
-            [255, 255, 0],  # Yellow
-            [0, 255, 0],  # Green
-        ]
-
-        result = np.zeros_like(image)
-
-        for i in range(n):
-            stage_mask = (normalized >= thresholds[i]) & (normalized < thresholds[i + 1])
-            result[stage_mask] = colors[i]
-
-        return result
+        colors = [[255, 0, 0], [255, 165, 0], [255, 255, 0], [0, 255, 0]]
+        colored_image = np.zeros_like(image)
+        for i, threshold in enumerate(thresholds[:-1]):
+            mask = (normalized >= threshold) & (normalized < thresholds[i + 1])
+            colored_image[mask] = colors[i]
+        return colored_image
